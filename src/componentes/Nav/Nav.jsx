b39f72244e2bcd57301252adf6e735b3fa0a6f01
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-09-16 22:49:22 -0300

    fix: 'fazendo navbar'

--- src/componentes/Nav/Nav.jsx
+++ src/componentes/Nav/Nav.jsx
@@ -1,15 +1,47 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import '../Nav/Nav.css'
 
-
-
-function Nav(){
-    return(
-        <>
-        <div className='div-nav'>
-            <h1>Nav</h1>
-        </div>
-        </>
-    )
+function Menu() {
+    return (
+        <Navbar expand="lg" className="nav-bar bg-body-tertiary mb-3">
+            <Container fluid>
+                <Navbar.Brand href="#" className='brand-link'>CODETECH</Navbar.Brand>
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                <Navbar.Offcanvas
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="end"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel">
+                            Mahindra Race
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="nav-canvas justify-content-start flex-grow-1 pe-3">
+                            <Nav.Link href="#action1" className='link ms-5 fs-5'>Home</Nav.Link>
+                            <Nav.Link href="#action2" className='link ms-5 fs-5'>Equipe</Nav.Link>
+                            <Nav.Link href="#action2" className='link ms-5 fs-5'>Contato</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Pesquisar..."
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-danger">Buscar</Button>
+                        </Form>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default Nav
+export default Menu;
